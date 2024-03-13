adfbafe64852ebb749ce7f7c0d8ea276c7f71757
Pedro Henrique Bergental <phbergental@brazpine.com>
2024-03-13 00:01:40 -0300

    chore: add NativeBase and configure layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { Center, Text, NativeBaseProvider, Spinner } from "native-base";
+import { useFonts, Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla';
+
+import { THEME } from './src/theme';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Center flex={1} background="gray.700">
+        {
+          fontsLoaded ?
+            <Text color="gray.100" fontFamily="heading">
+              Welcome to Marketspace!
+            </Text>
+          :
+            <Spinner color="gray.100" />
+        }
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
